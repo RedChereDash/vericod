0fda4587573938fc9e37cd56f1eab306037ac595
Gleb Salaimov <glebstim@gmail.com>
2025-03-12 22:27:55 +0700

    Версия для Доржу

--- testall.sv
+++ testall.sv
@@ -1,8 +1,27 @@
-module cnt_width_testbench; #(
-    localparam 
-) (
-    
-);
-    
-endmodule 
+module test_bench
+localparam cnt_width = 4;
+reg clk;
+reg [cnt_width:0] cnt_taktov,
 
+reg clk_div2;
+reg [7:0] byte_1;
+
+initial begin
+    clk = 0;
+    cnt_taktov = 0;
+end
+
+always begin
+    #10 clk = ~clk;
+    always @(posedge clk) begin
+        cnt_taktov <= cnt_taktov + 1;
+    end
+
+    wire reset = (cnt_taktov==3);
+    wire start = (cnt_taktov==5);
+    wire active;
+
+    trigger1 trg1(.clk(clk), .reset(reset), byte_1[0], );
+    trigger1 trg2(.clk(clk), .reset(reset),);
+end
+endmodule
