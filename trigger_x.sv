13255bc447986271f8ae84c7c2e3b05624fb2636
Gleb Salaimov <glebstim@gmail.com>
2025-03-12 22:05:37 +0700

    Ну какой то ещё файлик появился

---
+++ trigger_x.sv
@@ -0,0 +1,16 @@
+module trigger_x
+ (input clk,
+ input reset,
+ input set,
+ output reg out
+ );
+ always @(posedge clk) begin // по положительному фронту генератора clk
+ if (reset) begin // если reset=1
+ out <= 0; // на выходе триггера 0
+ end else begin // иначе
+ if (set) begin // если set=1
+ out <= 1; // на выходе триггера 1
+ end
+ end
+ end
+endmodule
